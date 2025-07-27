088173806cc15a5285a9b4e990bf66cd0378eb05
rafiharli <135038628+rafiharli@users.noreply.github.com>
2025-07-27 21:02:16 +0700

    Update web.py

--- web.py
+++ web.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import gdown
+import os
+
 
 # Konfigurasi halaman
 st.set_page_config(page_title="Movie Recommender System", layout="wide")
