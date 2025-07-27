1cabace054bcc5f1d8fb18530775d1991c1ccdce
rafiharli <135038628+rafiharli@users.noreply.github.com>
2025-07-27 15:27:35 +0700

    Update web.py

--- web.py
+++ web.py
@@ -15,9 +15,9 @@ def download_from_gdrive(file_id, output):
         gdown.download(url, output, quiet=False)
 
 # --- FILE ID DARI GOOGLE DRIVE ---
-knn_model_id = 'https://drive.google.com/file/d/1J_GkdoyKh30JLsCnHgM9r9Zdd9J5lSFv/view?usp=drive_link'
-knn_sim_matrix_id = 'https://drive.google.com/file/d/1Fjr_pCFcgRUF4XZDZnQLL6_B-dDw7fXX/view?usp=drive_link'
-svd_sim_matrix_id = 'https://drive.google.com/file/d/1RpcyDaSJVYusIu9mGn-gcqU5rwgYIT-w/view?usp=drive_link'
+knn_model_id = 'https://drive.google.com/uc?id=1J_GkdoyKh30JLsCnHgM9r9Zdd9J5lSFv'
+knn_sim_matrix_id = 'https://drive.google.com/uc?id=1Fjr_pCFcgRUF4XZDZnQLL6_B-dDw7fXX'
+svd_sim_matrix_id = 'https://drive.google.com/uc?id=1RpcyDaSJVYusIu9mGn-gcqU5rwgYIT-w'
 
 # --- DOWNLOAD FILE JIKA BELUM ADA ---
 download_from_gdrive(knn_model_id, 'knn_model.pkl')
