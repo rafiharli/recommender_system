f75243cd5c10d3d8abe10a873d56d67db6eea397
rafiharli <135038628+rafiharli@users.noreply.github.com>
2025-07-27 17:26:36 +0700

    Update web.py

--- web.py
+++ web.py
@@ -66,7 +66,6 @@ movies_df = movies_df[movies_df['movieId'].isin(available_movie_ids)]
 
 # Sidebar navigasi
 page = st.sidebar.selectbox("Navigasi", ["Halaman Awal", "Rekomendasi Film"])
-alpha = st.sidebar.slider("Nilai α (kontribusi SVD vs KNN)", 0.0, 1.0, 0.8, step=0.05)
 
 # 🏠 Halaman Awal
 if page == "Halaman Awal":
@@ -82,7 +81,7 @@ if page == "Halaman Awal":
 # 🎯 Halaman Rekomendasi
 elif page == "Rekomendasi Film":
     st.title("🎯 Cari Rekomendasi Film")
-
+    alpha = st.sidebar.slider("Nilai α (kontribusi SVD vs KNN)", 0.0, 1.0, 0.8, step=0.05)
     selected_title = st.selectbox("Ketik atau pilih judul film:", movies_df['title'].sort_values().unique())
     selected_movie = movies_df[movies_df['title'] == selected_title].iloc[0]
     selected_movie_id = int(selected_movie['movieId'])
